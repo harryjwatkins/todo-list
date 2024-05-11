10f937c3a4cd4da2f07d1765923d2a13ac5d1939
Harry Watkins <harry.jw@outlook.com>
2024-05-11 16:53:03 +0100

    Add TaskList

--- task.py
+++ task.py
@@ -2,3 +2,8 @@ class Task:
     def __init__(self, title, description) -> None:
         self.title = title
         self.description = description
+
+class TaskList:
+    def __init__(self, list_name, list_of_tasks) -> None:
+        self.list_name = list_name
+        self.list_of_tasks = list_of_tasks
