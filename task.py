b2eb409425dfec46ac4dc4cb0e3e43ed8c912d77
Harry Watkins <harry.jw@outlook.com>
2024-05-12 21:39:16 +0100

    Add typing and add_task method

--- task.py
+++ task.py
@@ -1,9 +1,15 @@
+from typing import List
+
 class Task:
-    def __init__(self, title, description) -> None:
+    def __init__(self, title: str, description: str) -> None:
         self.title = title
         self.description = description
 
 class TaskList:
-    def __init__(self, list_name, list_of_tasks) -> None:
+    def __init__(self, list_name: str, list_of_tasks: List[Task]) -> None:
         self.list_name = list_name
         self.list_of_tasks = list_of_tasks
+
+    def add_task(self, task: Task) -> None:
+        self.list_of_tasks.append(task)
+        print("Task was added to TaskList: {self.list_name}")
