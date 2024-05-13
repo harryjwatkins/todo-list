e29783b4e555973ba6a34e1eb8227235bdd884ab
Harry Watkins <harry.jw@outlook.com>
2024-05-13 23:06:18 +0100

    Add str methods

--- task.py
+++ task.py
@@ -5,10 +5,19 @@ class Task:
         self.title = title
         self.description = description
 
+    def __str__(self) -> str:
+        return f"Task is: {self.title}"
+
 class TaskList:
     def __init__(self, list_name: str, list_of_tasks: List[Task]) -> None:
         self.list_name = list_name
         self.list_of_tasks = list_of_tasks
+    
+    def __str__(self) -> str:
+        res = f"{self.list_name}:\n"
+        for task in self.list_of_tasks:
+            res += str(task) + "\n"
+        return res
 
     def add_task(self, task: Task) -> None:
         self.list_of_tasks.append(task)
--- todo-list.py
+++ todo-list.py
@@ -2,4 +2,4 @@ from task import *
 
 task_list = TaskList("TestList", [])
 task_list.add_task(Task("Test Task", "Test Description"))
-print(task_list.list_of_tasks[0].title)
+print(task_list)
