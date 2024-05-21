5a7f5fb11c94cad4adf0b016f5d4cde2aae0e8d4
Harry Watkins <harry.jw@outlook.com>
2024-05-21 21:57:23 +0100

    Add main menu

--- todo-list.py
+++ todo-list.py
@@ -1,7 +1,21 @@
 from classes.task import *
 from classes.tasklist import *
 
+def display_main_menu():
+    print("What would you like to do:")
+    print("1) Create task list")
+    print("2) View task lists")
+    print("3) Quit program")
+
+def get_main_menu_choice():
+    while True:
+        user_input = input("Please enter your choice:")
+        if user_input not in ["1", "2", "3"]:
+            print("Invalid input")
+            continue
+        return user_input
+
 user_task_lists = []
-name = input("Please enter the name of your task list")
-task_list = TaskList(name)
-print(task_list)
+while True:
+    display_main_menu()
+    user_choice = get_main_menu_choice()
