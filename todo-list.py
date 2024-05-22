7210c667aa461d5cf4adff1a76337cd770e4f906
Harry Watkins <harry.jw@outlook.com>
2024-05-22 21:40:58 +0100

    Add controller method to choose main menu option

--- todo-list.py
+++ todo-list.py
@@ -15,7 +15,23 @@ def get_main_menu_choice():
             continue
         return user_input
 
+def view_task_lists():
+    raise NotImplementedError()
+
+def create_task_list():
+    raise NotImplementedError()
+
+def select_main_menu_option(choice: str) -> None:
+    if choice == '1':
+        create_task_list()
+    elif choice == '2':
+        view_task_lists()
+    else:
+        print("Thanks for using this program, goodbye")
+        exit()
+
 user_task_lists = []
 while True:
     display_main_menu()
     user_choice = get_main_menu_choice()
+    select_main_menu_option(user_choice)
